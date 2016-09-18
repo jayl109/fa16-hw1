3c2299c5b42d1c9a3e75b2b5bb1bd24828f27add
jayl109 <jthemathfreak@gmail.com>
2016-09-17 23:24:33 -0700

    Update hw1.rb

--- hw1.rb
+++ hw1.rb
@@ -1,18 +1,17 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-  (a + b) ** 2
+  return a**2 + b**2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-  a.sort!.map do |b|
-    b + 1
-  end
+  
+  return a.map! { |a| a+1 }
+  
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-  "#{first_name} #{last_name}"
+  return a+b
 end
 
 def blockin_time(a)
